fea101092b2a05cbc5c86e24e3063bcfaa534a7a
Daniel <35694200+danielTobon43@users.noreply.github.com>
2019-04-09 11:19:01 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,16 +6,28 @@
 
 
 // Colors to display the generated clusters
-float colors[] = {  255,0,0,
-                    0,255,0,
-                    0,0,255,
-                    255,255,0,
-                    0,255,255,
-                    255,0,255,
-                    0,0,255,
-                    0,255,255,
-                    255,255,255
-                    };
+float colors[] = {  255,0,0,  //red 		1
+                    0,255,0,  //green		2
+                    0,0,255,  //blue		3
+                    255,255,0,//yellow		4
+                    0,255,255,//light blue	5
+                    255,0,255,//magenta     6               
+                    255,255,255,//white		7
+                    255,128,0,//orange		8
+                    255,153,255,//pink		9
+                    51,153,255,	//			10	
+                    153, 102, 51,//			11
+                    128, 51, 153,//			12
+                    153, 153, 51,//			13
+                    163, 38, 51,//			14
+                    204, 153, 102,//		15	
+                    204, 224, 255,//		16
+                    128, 179, 255,//		17
+                    206, 255, 0,//			18
+                    255,204,204,//			19
+                    204,255,153,//			20
+
+                 }; //20x3=60 color elements
 
 void readCloudFromFile(int argc, char** argv, std::vector<htr::Point3D>& points,
                        pcl::PointCloud<pcl::PointXYZRGB>::Ptr& cloud){
@@ -342,7 +354,7 @@ void init(int argc, char** argv,bool show){
           uint8_t g;
           uint8_t b; 
           
-          if(j < 27){
+          if(j < 60){
           
             r = (uint8_t) colors[j];
             g = (uint8_t) colors[j+1];
@@ -352,8 +364,9 @@ void init(int argc, char** argv,bool show){
             
             r = (uint8_t) uniform_0_255(gen);
             g = (uint8_t) uniform_0_255(gen);
-            b = (uint8_t) uniform_0_255(gen);  
-          
+            b = (uint8_t) uniform_0_255(gen); 
+
+                      
           }       
    
 
