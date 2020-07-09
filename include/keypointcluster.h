5382257ea735449c583e06cbdb75c8290b30752a
glupschi08 <florian.auinger@gmail.com>
2020-07-09 17:00:04 +0200

    add class descriptor

--- include/keypointcluster.h
+++ include/keypointcluster.h
@@ -2,7 +2,29 @@
 // Created by florian on 09/07/20.
 //
 
+#pragma once
+#include <Eigen/Eigen>
+
 #ifndef DBSCAN_KEYPOINTCLUSTER_H
 #define DBSCAN_KEYPOINTCLUSTER_H
+
+
+class KeypointCluster{
+public:
+    int ClusterID;
+    int ClusterSize;
+    //Eigen::Matrix3f& Eigenvectors;
+    //Eigen::Vector3f& Eigenvalues;
+    float minX;
+    float minY;
+    float minZ;
+    float maxX;
+    float maxY;
+    float maxZ;
+    //void set_values(int,int,Eigen::Matrix3f&, Eigen::Vector3f&, float,float,float,float,float,float);
+    void set_values(int,int, float,float,float,float,float,float);
+
+};
+
 
 #endif //DBSCAN_KEYPOINTCLUSTER_H
--- src/keypointcluster.cpp
+++ src/keypointcluster.cpp
@@ -2,3 +2,21 @@
 // Created by florian on 09/07/20.
 //
 
+#include <stdlib.h>
+#include <iostream>
+#include "keypointcluster.h"
+
+//void KeypointCluster::set_values (int x_id, int y_size,Eigen::Matrix3f& e_vecs,Eigen::Vector3f& e_vals, float a, float b, float c, float d, float e, float f) {
+void KeypointCluster::set_values (int x_id, int y_size, float a, float b, float c, float d, float e, float f) {
+
+    ClusterID=x_id;
+    ClusterSize=y_size;
+    //Eigenvectors=e_vecs;
+    //Eigenvalues=e_vals;
+    minX = a;
+    minY = b;
+    minZ = c;
+    maxY = d;
+    maxX = e;
+    maxZ = f;
+}
