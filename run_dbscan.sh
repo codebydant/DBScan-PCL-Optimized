f16c93e1bfd76de78a10a6a05ba77ae0865b3b36
Daniel Tobon <daniel.tobon@uao.edu.co>
2022-06-28 16:39:58 -0500

    new test

--- run_dbscan.sh
+++ run_dbscan.sh
@@ -4,7 +4,8 @@ docker run -it --rm \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --volume=/home/danieltc/Downloads/models:/tmp \
-    danieltobon43/dbscan:1.1-alpine3.15 --cloudfile $1
+    --volume=/home/danieltc/Downloads/DBScan-PCL-Optimized:/tmp \
+    --entrypoint /bin/sh \
+    ghcr.io/danieltobon43/dbscan-pcl-optimized:test-github-packages
 # Disallow X server connection
 xhost -local:root
