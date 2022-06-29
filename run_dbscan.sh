58f158f183856f8ba6a8c8d88ffa084883c528e2
Daniel Tobon <daniel.tobon@uao.edu.co>
2022-06-29 06:05:58 -0500

    replaced docker image tag to latest

--- run_dbscan.sh
+++ run_dbscan.sh
@@ -1,11 +1,10 @@
+# xhost -local:root
 # Allow X server connection
 xhost +local:root
 docker run -it --rm \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --volume=/home/danieltc/Downloads/DBScan-PCL-Optimized:/tmp \
-    --entrypoint /bin/sh \
-    ghcr.io/danieltobon43/dbscan-pcl-optimized:test-github-packages
+    ghcr.io/danieltobon43/dbscan-octrees:latest
 # Disallow X server connection
 xhost -local:root
