cd2baa7de2cf36847b7d4955c41a88555c031e7e
Daniel Tobon <danieltobonco@gmail.com>
2022-07-06 14:11:45 -0500

    test 117

--- run_dbscan.sh
+++ run_dbscan.sh
@@ -5,9 +5,12 @@ docker run -it --rm \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --volume=/home/danieltc/Downloads/models:/tmp/output \
+    --entrypoint=/bin/sh \
     ghcr.io/danieltobon43/dbscan-octrees:latest
     # --entrypoint=/bin/sh \
     # --volume=`pwd`:/tmp/output \
+        # --volume=/home/danieltc/Downloads/models:/tmp/output \
     
     # --entrypoint=/bin/sh \
     # --volume=`pwd`:/tmp \
