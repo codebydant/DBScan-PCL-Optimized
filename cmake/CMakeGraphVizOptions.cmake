43e945b4260414978407fb1ef53e23be44cbe93b
Daniel Tobon <danieltobonco@gmail.com>
2022-07-12 14:24:54 -0500

    added cmake dependency graph for project

---
+++ cmake/CMakeGraphVizOptions.cmake
@@ -0,0 +1,8 @@
+# https://cmake.org/cmake/help/latest/module/CMakeGraphVizOptions.html
+set(GRAPHVIZ_UNKNOWN_LIBS OFF)
+set(GRAPHVIZ_EXTERNAL_LIBS ON)
+set(GRAPHVIZ_OBJECT_LIBS OFF)
+set(GRAPHVIZ_STATIC_LIBS OFF)
+set(GRAPHVIZ_GENERATE_DEPENDERS ON)
+set(GRAPHVIZ_GENERATE_PER_TARGET ON)
+set(GRAPHVIZ_IGNORE_TARGETS "VTK" "Boost" "dl" "libGL" "libexecinfo" "Threads")
